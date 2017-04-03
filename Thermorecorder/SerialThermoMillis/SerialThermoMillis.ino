3206bf131d3f8b2ce484fe548b22330eda3388bd
zoomx <zoomx@tiscalinet.it>
2017-04-03 17:20:53 +0200

    Added SerialThermo example with millis instead of delay

---
+++ Thermorecorder/SerialThermoMillis/SerialThermoMillis.ino
@@ -0,0 +1,48 @@
+/*
+   SerialThermoMillis
+   Original by Adafruit
+   this example is public domain. enjoy!
+   https://learn.adafruit.com/thermocouple/
+*/
+#include "max6675.h"
+
+const int thermoDO = 4;
+const int thermoCS = 5;
+const int thermoCLK = 6;
+
+MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
+
+const int vccPin = 3;
+const int gndPin = 2;
+
+
+unsigned long previousMillis = 0;
+unsigned long currentMillis = 0;
+unsigned long intervallo = 2500;
+bool NewData;
+//******************************************************
+void CheckTime() {
+  if ((millis() - previousMillis) > intervallo) {
+    Serial.print("C = ");
+    Serial.println(thermocouple.readCelsius());
+    previousMillis = millis();
+  }
+}
+//******************************************************
+void setup() {
+  Serial.begin(115200);
+  Serial.println("SerialThermoMillis");
+  pinMode(vccPin, OUTPUT); digitalWrite(vccPin, HIGH);
+  pinMode(gndPin, OUTPUT); digitalWrite(gndPin, LOW);
+
+  Serial.println("MAX6675 test");
+  // wait for MAX chip to stabilize
+  delay(500);
+  Serial.print("C = ");
+  Serial.println(thermocouple.readCelsius());
+  previousMillis = millis();
+}
+//******************************************************
+void loop() {
+  CheckTime();
+}
